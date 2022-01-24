1cfebae485e16b5e97192a404a03984366135981
PrinceNiyonshuti <npprince47@gmail.com>
2022-01-24 10:26:15 +0200

    implemented the use of Queue to send notifications in the background

--- app/Notifications/NotifyAdmin.php
+++ app/Notifications/NotifyAdmin.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class NotifyAdmin extends Notification
+class NotifyAdmin extends Notification implements ShouldQueue
 {
     use Queueable;
     private $notificationData;
