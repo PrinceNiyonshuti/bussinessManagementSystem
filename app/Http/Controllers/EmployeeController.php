96f099521b0cbe902d6c080ea9f77231323adfc3
PrinceNiyonshuti <npprince47@gmail.com>
2022-01-28 13:34:41 +0200

    added procfile

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -18,6 +18,7 @@ class EmployeeController extends Controller
             'employees' => Employee::latest()->paginate(10)
         ]);
     }
+    
     public function create()
     {
         return view('admin.employee.create');
